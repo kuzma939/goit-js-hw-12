37ce1652e90ccfba3e1a6e52578107ea0f91667a
kuzma939 <135020134+kuzma939@users.noreply.github.com>
2024-04-17 14:50:58 +0200

    goit-js-hw-12

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-
 export async function objGallery(page, names) {
     const BASE_URL = `https://pixabay.com/api/`;
     const API_KEY = `43257905-28a3b58ba6106b31a5e4f67d7`;
@@ -14,7 +13,4 @@ export async function objGallery(page, names) {
     })
     const {data} = await axios(`${BASE_URL}?${PARAMS}`); 
    return data;
-    
-
-    
-}
+};
